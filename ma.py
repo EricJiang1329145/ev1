51fce3a60d3b35fd3591ab743c73efb7c3d32217
EricJiang1329145 <jmr_eric@outlook.com>
2025-03-09 10:56:04 +0800

    暂时改用在线deepseek服务，测试环境并重建

--- ma.py
+++ ma.py
@@ -1,2 +1,16 @@
-import torch
-print(torch.backends.mps.is_available())  # 应返回True
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-42576b8258364c2e8f350e511708e767", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
